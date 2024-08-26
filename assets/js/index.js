290b356098140dd6f947ce421b4f686f0f137cd7
Christopher Nunez <elgary1129@gmail.com>
2024-08-26 16:13:57 -0400

    Funcionalidad del boton copiar y mostrar el texto encriptado y desencriptado por pantalla listos

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,6 +2,8 @@ const form = document.querySelector('#form');
 const btnEncriptar = document.querySelector('.encriptar');
 const btnDesencriptar = document.querySelector('.desencriptar');
 const aside = document.querySelector('#aside');
+const mensaje = document.querySelector('.mensaje');
+const btnCopiar = document.querySelector('.btn-copiar');
 
 
 function encriptar() {
@@ -9,8 +11,29 @@ function encriptar() {
     const textoSinAcentos = normalizarTexto(texto);
     // Reemplaza los caracteres del primer parametro de la funcion por el segundo incluyendo si son letras mayúsculas
     const textoEncriptado = textoSinAcentos.replace(/u/gi, 'ufat').replace(/e/gi, 'enter').replace(/o/gi, 'ober').replace(/i/gi, 'imes').replace(/a/gi, 'ai');
-    console.log(textoEncriptado)
-    return textoEncriptado;
+    mensaje.setAttribute('style', 'display:flex')
+    btnCopiar.setAttribute('style', 'display:flex')
+    return mensaje.textContent = textoEncriptado
+}
+
+function desencriptar() {
+    const texto = document.querySelector('#texto').value;
+    // Reemplaza los caracteres del primer parametro de la función por el segundo incluyendo si son letras mayúsculas
+    // (En está función hago lo mismo que en la función encriptar solo que invierto la conversion de caracteres)
+    const textoDesencriptado = texto.replace(/ai/gi, 'a').replace(/enter/gi, 'e').replace(/imes/gi, 'i').replace(/ober/gi, 'o').replace(/ufat/gi, 'u');
+    btnCopiar.setAttribute('style', 'display:flex')
+    return mensaje.textContent = textoDesencriptado
+}
+
+function copiar(newClip){
+    navigator.clipboard.writeText(newClip).then(
+        () => {
+          
+        },
+        () => {
+          
+        },
+      );
 }
 
 //Funcion para sacar los acentos de las letras
@@ -18,6 +41,15 @@ function normalizarTexto(texto) {
     return texto.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
 }
 
+function sacarImagenAside(){
+    const img = document.querySelector('.img');
+    const msjEncriptado = document.querySelector('.msj-encriptado');
+
+    img.setAttribute('style', 'display:none');
+    msjEncriptado.setAttribute('style', 'display:none');
+
+    
+}
 
 //Función para cambiar el texto cuando ocurra una modificación
 function mostrarCodigo(texto){
@@ -30,17 +62,12 @@ function reset() {
     form.reset();
 }
 
-function desencriptar() {
-    const texto = document.querySelector('#texto').value;
-    // Reemplaza los caracteres del primer parametro de la función por el segundo incluyendo si son letras mayúsculas
-    // (En está función hago lo mismo que en la función encriptar solo que invierto la conversion de caracteres)
-    const textoDesencriptado = texto.replace(/ai/gi, 'a').replace(/enter/gi, 'e').replace(/imes/gi, 'i').replace(/ober/gi, 'o').replace(/ufat/gi, 'u');
-    return textoDesencriptado;
-}
+
 
 //Añado los eventos onClick en los respectivos botones
 
 btnEncriptar.addEventListener('click', function () {
+    sacarImagenAside()
     mostrarCodigo(encriptar())
     reset()
 });
@@ -49,3 +76,8 @@ btnDesencriptar.addEventListener('click', function () {
     mostrarCodigo(desencriptar())
     reset()
 });
+
+btnCopiar.addEventListener('click', function(){
+    const textoCopiado = document.querySelector('.mensaje').value;
+    copiar(textoCopiado);
+})
